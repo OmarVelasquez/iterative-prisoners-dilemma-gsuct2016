46338823fcc81785d7b9c64232842481a31c4810
OmarVelasquez <024988@daltonk12.onmicrosoft.com>
2017-02-21 14:33:58 -0500

    Strategy
    team_name = 'Hacks4Hire' # Only 10 chars displayed.
    strategy_name = 'Test of Faith'
    strategy_description = 'On the first found it betrays and if the opponet colludes then it colludes but if the oponet colludes two time in a row then it betrays'
        
    def move(my_history, their_history, my_score, their_score):
        ''' Arguments accepted: my_history, their_history are strings.
        my_score, their_score are ints.'''
        if len(my_history) == 0:
            return 'b'
        elif their_history[-1] == 'b':
            return 'b'
        elif their_history[-1] == 'c':
            if len(their_history)>= 2:
                if their_history[-2] == 'c':
                    return 'b'
            else:
                return 'c'
        '''Make my move.
        Returns 'c' or 'b'. 
        '''

--- team9.py
+++ team9.py
@@ -7,26 +7,25 @@
 ####
 
 team_name = 'Hacks4Hire' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Test of Faith'
+strategy_description = 'On the first found it betrays and if the opponet colludes then it colludes but if the oponet colludes two time in a row then it betrays'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
-    
-    Make my move.
+    my_score, their_score are ints.'''
+    if len(my_history) == 0:
+        return 'b'
+    elif their_history[-1] == 'b':
+        return 'b'
+    elif their_history[-1] == 'c':
+        if len(their_history)>= 2:
+            if their_history[-2] == 'c':
+                return 'b'
+        else:
+            return 'c'
+    '''Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
